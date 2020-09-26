cb5189485975b19589af2832e536c8bb50aabdd6
Mauricio Ojeda <mauricioojeda@Mauricios-Mac-mini.local>
2020-09-26 13:59:11 -0300

    objspreadoperator

--- objspreadoperator/main.js
+++ objspreadoperator/main.js
@@ -1,1 +1,25 @@
 // object spread operator
+/**
+ * copia objetos en otros sin mutar los origines
+*/
+const obj = {
+    a: 1,
+    b: 2
+}
+
+const obj1 = {
+    ...obj
+}
+obj1['c'] = 3
+
+const obj2 = {
+    data: {
+        ...obj
+    }
+}
+
+obj2['c'] = 3
+
+
+console.log(obj, obj1);
+console.log(obj2);
